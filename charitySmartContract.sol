f69473a67210e896c6db6180ae0d4026823ed16c
Ecjoe <43061889+Ecjoe@users.noreply.github.com>
2020-11-07 00:30:29 +0530

    Create charitySmartContract.sol

---
+++ charitySmartContract.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.5.13 < 0.7.3;
+
+contract charity_smart_contract{
+    
+    address public owner;
+    uint public my_balance;
+    bool public isPause;
+    
+    struct paymentdetails{
+        address donor;
+        uint amount;
+    }
+    
+    constructor() public{
+        owner=msg.sender;
+    }
+    
+    function receiveEthers() public payable{
+        my_balance += msg.value;
+        
+    }
+    
+    function transferEthers(address payable _needy) public payable ownerAllowed{
+        _needy.transfer(address(this).balance);   
+    }
+    
+    function setpause(bool _isPause)  public ownerAllowed ownerpause{
+        isPause=_isPause;
+    }
+    
+    function setOwner(address _newOwner) public ownerAllowed {
+        
+        owner=_newOwner;
+        
+    }
+    modifier ownerAllowed(){
+        require(msg.sender== owner,"owner has access to this");
+        _;
+    }
+    modifier ownerpause(){
+        require(!isPause,"owner has access to this ssaa");
+        _;
+    }
+}
+
